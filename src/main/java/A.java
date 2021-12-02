bd22c30cb7136b623843840112dd2384d286e8ab
wszpan <wszpan@126.com>
2021-12-02 14:21:13 +0800

    create sub()

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,5 +1,9 @@
 public class A {
-	public void add(int a,int b){
-		System.out.println("a+b = " + (a+b));
+	public void add(int a, int b) {
+		System.out.println("a + b = " + (a + b));
+	}
+
+	public void sub(int a, int b) {
+		System.out.println("a - b = " + (a - b));
 	}
 }
