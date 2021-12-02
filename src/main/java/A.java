a34710899c2ab5aa11ff73478985707f59c3bdf3
wszpan <wszpan@126.com>
2021-12-02 14:18:52 +0800

    create add()

---
+++ src/main/java/A.java
@@ -0,0 +1,5 @@
+public class A {
+	public void add(int a,int b){
+		System.out.println("a+b = " + (a+b));
+	}
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MainTest {
+	@Test
+	public void add(){
+		A a = new A();
+		a.add(10,13);
+	}
+}
